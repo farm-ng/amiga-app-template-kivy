c24433edebe9d7d71aaa554e3bf36140c82a5854
Christopher Bradski <chris@farm-ng.com>
2022-11-03 15:01:34 -0700

    updated default required args in entry.sh

--- apps/virtual_joystick/entry.sh
+++ apps/virtual_joystick/entry.sh
@@ -3,6 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $DIR/../../bootstrap.sh $DIR $DIR/venv
 
-$DIR/venv/bin/python $DIR/main.py --port 50060 # ${1} ${2} ${3} ${4}
+$DIR/venv/bin/python $DIR/main.py \
+    --camera-port 50051 \
+    --canbus-port 50060
 
 exit 0
