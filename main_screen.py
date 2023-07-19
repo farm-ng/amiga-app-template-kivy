9e5496fff689995eb21cad9461405eeaa89390bd
edgarriba <edgar.riba@gmail.com>
2023-07-18 17:26:47 -0700

    WIP: select files from file prompt

--- main_screen.py
+++ main_screen.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.properties import StringProperty
@@ -8,7 +10,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.tabbedpanel import TabbedPanelItem
 from utils import FileDescription, get_list_of_files
-from pathlib import Path
 
 Builder.load_string(
     """
@@ -41,7 +42,6 @@ Builder.load_string(
                 pos_hint: {'top': 1.0, 'right': 1.0}
                 size_hint: 0.1, 0.1
                 on_release: root.on_tab_action_button()
-
 <FilesTabbedPanel>:
     ScrollView:
         id: scroll_view
@@ -51,13 +51,15 @@ Builder.load_string(
             cols: 1
             size_hint_y: None
             height: self.minimum_height
-
 <FileDescriptionWidget@BoxLayout>:
     orientation: 'horizontal'
     size_hint_y: None
-    Label:
+    Button:
+        id: root.id
         text: root.text
+        on_release: root.printing(root.text, root.id)
 """
+
 )
 
 # ui classes
@@ -68,22 +70,36 @@ class FilesTabbedPanel(TabbedPanelItem):
 
 
 class FileDescriptionWidget(BoxLayout):
+    id = StringProperty()
     text = StringProperty()
-
+    
+    def printing(self, text, id):
+        print('Hello, you tapped a button', text)
+        print('id is', id)
+        print('Loading other file')
+        
+        # TODO URGENT Find a better way to change screens 
+        # self.parent.parent.parent.parent.parent.parent.parent.parent.current = "boundary_screen"
+        # print(self.parent)
+    
 
 class MainScreen(Screen):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         # initialize tabbed panel
+        # methods = [method_name for method_name in dir(MainScreen) if callable(getattr(MainScreen, method_name))]
+        # print(methods)
+        
         self.update_tabs_scroll_views(self.app.guide_path)
 
     @property
     def app(self) -> App:
         """Get app instance."""
         return App.get_running_app()
+    
 
     # TODO: improve this later, now just to prove concept
-    def update_tabs_scroll_views(self,path) -> None:
+    def update_tabs_scroll_views(self, path) -> None:
         """Update tabbed panel scroll views."""
         # get list of files to update
         print(path)
@@ -103,15 +119,14 @@ class MainScreen(Screen):
             for file in files:
                 tab_grid_layout.add_widget(
                     FileDescriptionWidget(
-                        text=f"{tab.text} -- {file.name} -- {file.size_bytes} bytes -- {file.modification_time_str}"
+                        id = file.name,
+                        text = f"{tab.text} -- {file.name} -- {file.size_bytes} bytes -- {file.modification_time_str}"
                     )
                 )
 
     def on_tab_action_button(self) -> None:
         """Handle tab action button press.
-
         This method is called when the tab action button is pressed and redirects to the correct screen.
-
         """
         # get current tab text
         current_tab_text: str = self.ids.tabbed_panel.current_tab.text.lower().replace(
