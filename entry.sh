e3b7431ef26f835b8f28b7b23e8a32d43cf1a4df
OLO <oliver@farm-ng.com>
2024-01-24 22:23:48 +0000

    custom app update

--- entry.sh
+++ entry.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $DIR/bootstrap.sh $DIR $DIR/venv
 
-$DIR/venv/bin/python $DIR/src/main.py $@
+DISPLAY=:0 $DIR/venv/bin/python $DIR/src/main.py $@
 
 exit 0
